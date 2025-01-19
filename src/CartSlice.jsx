afc1c4b96f2d7a1c31280b08a49c151f7f182cc5
deebika-b <52105272+deebika-b@users.noreply.github.com>
2025-01-19 18:35:22 +0100

    Update CartSlice.jsx

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,3 +1,4 @@
+
 import { createSlice } from '@reduxjs/toolkit';
 
 export const CartSlice = createSlice({
@@ -9,7 +10,7 @@ export const CartSlice = createSlice({
     
     addItem: (state, action) => {
         const { name, image, cost } = action.payload;
-        const existingItem = state.items.find(item => item.name == name);
+        const existingItem = state.items.find(item => item.name === name);
         if (existingItem ) {
             existingItem.quantity++;
         } else {
@@ -36,3 +37,4 @@ export const CartSlice = createSlice({
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
 
 export default CartSlice.reducer;
+
